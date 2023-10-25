cc648543d05b5e275c956d402a4fdbbfca682962
AndriiKozlovskyi <andrii@advasoftware.com>
2023-10-25 21:40:36 +0200

    super UI improvements

--- cmc_parser/src/App.tsx
+++ cmc_parser/src/App.tsx
@@ -1,29 +1,25 @@
-import { useState } from 'react'
-// import OkxParse from './api/okx/okx_parse';
-// import Splitter from './api/splitter/splitter';
-import './App.css'
-import HuobiParse from './api/huobi/huobi_parse';
-import ExchangeParser from './api/exchange_parser';
+import "./App.css";
+import HuobiParse from "./api/huobi/huobi_parse";
+import ExchangeParser from "./api/exchange_parser";
 
 function App() {
-  const [res, setRes] = useState("");
-
   async function callApi() {
-    
-    // const respose = await Splitter.split();
-    // setRes(JSON.stringify(await BinanceParse.parseOrderBookTradingSymols()));  
     const parse: ExchangeParser = new HuobiParse();
     console.log(await parse.parseOrderBookTradingSymols());
   }
 
   return (
     <>
-    <div className="font-bold underline" onClick={callApi}>
-      MNIUYVHIOJPIUVY
-      {res}
-    </div>
+      <div className="h-screen w-screen flex justify-center items-center">
+        <button
+          className="font-bold bg-indigo-300 rounded-2xl hover:bg-indigo-400 shadow-xl px-3 py-1"
+          onClick={callApi}
+        >
+          Run devil machine!
+        </button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
